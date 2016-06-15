0a72694149d5ce74f1f721b575eced1f389758e2
工程师查理 <charlie@cokapp.com>
2016-06-15 18:07:16 +0800

    增加api

--- client/src/app/scripts/services/dockerApiSvr.js
+++ client/src/app/scripts/services/dockerApiSvr.js
@@ -6,7 +6,7 @@
 
         var svr = {};
 
-        svr.loadInfo = function(cb) {
+        svr.info = function(cb) {
             var url = 'http://ub.home:2375/info';
             var p = $http({
                 method: 'GET',
@@ -15,7 +15,19 @@
             p.success(function(rsp) {
                 cb(rsp);
             });
-        }
+        };
+
+        svr.containers = function(cb) {
+            var url = 'http://ub.home:2375/containers/json';
+            var p = $http({
+                method: 'GET',
+                url: url
+            });
+            p.success(function(rsp) {
+                cb(rsp);
+            });
+        };
+
 
         return svr;
     });
