520d57979a52deb47c0c29a23a53441b72ce15b3
查理 <charlie@cokapp.com>
2016-06-23 16:49:08 +0800

    支持console了

--- client/src/app/scripts/controllers/containerCtrl.js
+++ client/src/app/scripts/controllers/containerCtrl.js
@@ -5,7 +5,7 @@
         'ngInject';
 
         $scope.id = $stateParams.id;
-
+        $scope.wsUrl = 'ws://ub.home:2375/containers/' + $stateParams.id + '/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1';
     });
 
 })();
