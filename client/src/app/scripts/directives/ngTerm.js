4d6da8295faf98469f24377b635a92bbf9644807
查理 <charlie@cokapp.com>
2016-07-07 17:23:54 +0800

    添加功能

--- client/src/app/scripts/directives/ngTerm.js
+++ client/src/app/scripts/directives/ngTerm.js
@@ -10,40 +10,60 @@
                 url: '=',
 
                 logs: '@',
-                stdin: '@'
+                stdin: '@',
+                start: '='
             },
             template: '<div class="ng-term"></div>',
             replace: false,
             link: function(scope, element, attrs) {
-                var term = new Terminal({
-                    cols: 100,
-                    rows: 20,
-                    convertEol: true,
-                    useStyle: true,
-                    cursorBlink: true,
-                    screenKeys: true
-                });
+                var inited = false;
 
-                term.open(element.find('.ng-term')[0]);
+                var init = function() {
+                    var term = new Terminal({
+                        cols: 100,
+                        rows: 20,
+                        convertEol: true,
+                        useStyle: true,
+                        cursorBlink: true,
+                        screenKeys: true
+                    });
+                    var ws = $websocket(scope.url);
 
-                var ws = $websocket(scope.url);
+                    ws.onMessage(function(message) {
+                        term.write(message.data);
+                    });
+                    ws.onClose(function() {
+                        term.destroy();
+                    });
+                    ws.send('\n');
 
-                ws.onMessage(function(message) {
-                    term.write(message.data);
-                });
-                ws.onClose(function() {
-                    term.destroy();
-                });
+                    term.open(element.find('.ng-term')[0]);
+                    term.on('data', function(message) {
+                        ws.send(message);
+                    });
 
-                term.on('data', function(message) {
-                    ws.send(message);
-                });
+                    element.on('$destroy', function() {
+                        term.destroy();
+                    });
 
-                element.on('$destroy', function() {
-                    term.destroy();
-                });
+                    inited = true;
+                }
 
-                ws.send('\n');
+                if (scope.start) {
+                    init();
+                }
+
+                if (!inited) {
+                    var stopWatch = scope.$watch('start', function() {
+                        if (scope.start) {
+                            init();
+                            stopWatch();
+                        }
+                    });
+                }
+
+
+
             }
         };
 
