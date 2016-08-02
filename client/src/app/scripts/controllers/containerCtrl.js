fb1fd5bf0657c49f3f0e20c4a4af2af8ff7de102
黑承亮 <heichengliang@aixuedai.com>
2016-08-02 17:48:44 +0800

    基本可行

--- client/src/app/scripts/controllers/containerCtrl.js
+++ client/src/app/scripts/controllers/containerCtrl.js
@@ -9,23 +9,9 @@
         $scope.attachUrl = 'ws://ct.home:2375/containers/' + $stateParams.id + '/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1';
         $scope.attachStart = true;
 
-        $scope.execStart = false;
-        dockerApiSvr.containers_exec($scope.id, function(rsp) {
-            var execId = rsp.Id;
 
-            //var url = 'http://ct.home:2375/exec/'+execId+'/start';
-
-
-            dockerApiSvr.exec_start(execId, function(rsp2) {
-                console.log('rsp2-------------------------------');
-                console.log(rsp2);
-
-
-
-                //$scope.execUrl = 'ws://ct.home:2375/exec/' + execId + '/start';
-                //$scope.execStart = true;
-            });
-        });
+        $scope.execUrl = 'ws://localhost:9000/exec';
+        $scope.execStart = true;
 
         //semantic初始化
         $element.find('.tabular.menu > .item').tab();
