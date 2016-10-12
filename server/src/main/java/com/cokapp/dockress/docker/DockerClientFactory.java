47279a7456e1dbbdaa864383d50d5b39d179a7f8
酷卡移动 <dev@cokapp.com>
2016-10-12 16:46:57 +0800

    docker完成

--- server/src/main/java/com/cokapp/dockress/docker/DockerClientFactory.java
+++ server/src/main/java/com/cokapp/dockress/docker/DockerClientFactory.java
@@ -3,11 +3,8 @@ package com.cokapp.dockress.docker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cokapp.cokits.core.lang.exception.ExceptionUtils;
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.DockerCmdExecFactory;
-import com.github.dockerjava.api.model.Version;
-import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.netty.DockerCmdExecFactoryImpl;
 
 /**
  * Docker客户端工厂
@@ -24,21 +21,36 @@ import com.github.dockerjava.netty.DockerCmdExecFactoryImpl;
 public class DockerClientFactory {
 	public static Logger logger = LoggerFactory.getLogger(DockerClientFactory.class);
 
-	public DockerClient buildDefault() {
-		DockerCmdExecFactory dockerCmdExecFactory = new DockerCmdExecFactoryImpl();
-		DockerClient dockerClient = DockerClientBuilder.getInstance().withDockerCmdExecFactory(dockerCmdExecFactory)
-				.build();
-		try {
-			Version version = dockerClient.versionCmd().exec();
-			logger.debug("version: {}", version.toString());
-			logger.debug("docker client inited!");
-		} catch (Exception e) {
-			logger.error("docker client init failed: \"{}\".", e.getMessage());
-		}
-
-		logger.debug("docker client inited!");
+	public DockerClient buildDefault() throws InterruptedException {
+		//step1. 新线程构建客户端
+		DockerBuilderTask task = new DockerBuilderTask();
+		Thread thread = new Thread(task);
+		thread.start();
 		
-		return dockerClient;
+		//step2. 等待最多10秒
+		int second = 0;
+		while(second < 10 && !task.isReady()){
+			Thread.sleep(1000);
+			second++;
+			logger.debug("已等待【{}】秒！", second);
+		}
+		
+		//step3. 返回client
+		if(task.isReady()){
+			logger.debug("共等待【{}】秒，初始化完成了，Docker版本是【{}】！", second, task.getDockerVersion());
+			return task.getDockerClient();
+		}
+		
+		ExceptionUtils.wrapBiz("初始化Docker客户端失败！");
+		return null;
 	}
+	
+	
+	
+
+	
+	
+	
+	
 
 }
